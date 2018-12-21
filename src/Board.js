3310c4387e233a5427a13e480cb59f8c0502c569
stevenkulp <sdkulp@gmail.com>
2018-12-21 14:51:25 -0800

    basic tic-tact-toe board

--- src/Board.js
+++ src/Board.js
@@ -20,18 +20,33 @@ class Board extends Component {
 
 //method that measures click to determines who's turn is it.
   teamSelect = (e) => {
-    let {playerOne, playerTwo} = this.state
-    this.setState({playerOne: playerOne, playerTwo: playerTwo})
+    let {playerOne, playerTwo, count} = this.state
+    //Set up what input was changed/typed in
+    let newPlayerEntry = e.target.id
+    //Get the value entered through input
+    let newPlayerSymbol = e.target.value
+    console.log(count);
+    // console.log(newPlayerEntry);
+    // console.log(newPlayerSymbol);
+    // playerTwo = ""
+    // console.log(player, side);
+    //Update state with new value
+    this.setState({[newPlayerEntry]: newPlayerSymbol})
+
   }
 
   //method for userClick
   userClick(e) {
-    let { spaces, count,playerOne, playerTwo, message} = this.state
-      if((count+2)% 2 === 0 && spaces[e.target.id] === "") {
-          spaces[e.target.id] = "X"
+    let { spaces, count, playerOne, playerTwo, message} = this.state
+    console.log(count);
+    console.log(playerOne);
+    console.log(playerTwo);
+
+      if(playerOne != "" && (count+2)% 2 === 0 && spaces[e.target.id] === "") {
+          spaces[e.target.id] = playerOne
           count++
-      } else if ((count+2)%2 === 1 && spaces[e.target.id] === "") {
-          spaces[e.target.id] = "O"
+      } else if (playerTwo != "" && (count+2)%2 === 1 && spaces[e.target.id] === "") {
+          spaces[e.target.id] = playerTwo
           count++
       }
   //centralized winning condition
@@ -39,11 +54,12 @@ class Board extends Component {
 
       let win= winningConditions.filter((combo,index,array) => {
       var [a,b,c] = combo
-      if(spaces[a]==="X" && spaces[b]==="X"&& spaces[c]==="X"){
-        message = "X is the Winner!"
-      } else if(spaces[a]==="O" && spaces[b]==="O"&& spaces[c]==="O"){
-        message = "O is the winner!"
-      } else if(count === 9){
+      if(spaces [a] && spaces[a]=== playerOne && spaces[b]===playerOne && spaces[c]=== playerOne){
+        message = `${playerOne} is the Winner!`
+      } else if(spaces [a] && spaces[a]=== playerTwo && spaces[b]=== playerTwo && spaces[c]=== playerTwo){
+        message = `${playerTwo} is the Winner!`
+      }
+      else if(count === 9){
         message = "It's a draw!"
       }
       })
@@ -52,7 +68,9 @@ class Board extends Component {
     }
 
   render() {
-    let { count,playerOneScore,message } = this.state
+    console.log(this.state);
+    let { count,message, playerOne, playerTwo } = this.state
+    // console.log(playerOne);
     let squares = this.state.spaces.map((val, index) => {
     return  (
       <div className = "grid-item" key={index} id={index} onClick={this.userClick.bind(this)}>{val}</div> )
@@ -61,22 +79,23 @@ class Board extends Component {
   })
   return (
     <div>
-    <div className="message"> {message}</div>
-    <div className="grid-container">
-      {squares}
-    </div>
-    <form onSubmit= {this.teamSelect}>
-    <button className="Button" type = "submit" > Restart  </button>
-    </form>
+      <div className="message"> {message}</div>
+      <div className="grid-container"> {squares} </div>
+      <div className="selector"> Select a Player</div>
+      <div className= "players">
+      <label>Player One:
+        <input className="team" type="text" id='playerOne' value={playerOne} onChange={this.teamSelect}/>
+      </label>
+      <label> Player Two:
+        <input className="team" type="text" id='playerTwo' value={playerTwo} onChange={this.teamSelect}/>
+      </label>
+      </div>
+
+      <form onSubmit= {this.teamSelect}>
+        <button className="Button" type = "submit" > Restart  </button>
+      </form>
     </div>
   );
 }
 }
 export default Board
-
-/*  <label> Player One:
-<input type="text" id='playerOne' value={this.playerOne} onChange={this.teamSelect}/>
-</label>
-<label> Player Two:
-<input type="text" id='playerTwo' value={this.playerTwo} onChange={this.teamSelect}/>
-</label> */
